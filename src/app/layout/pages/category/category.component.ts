e75c46ef43ada0f3046b4c09c5f629c7569fee80
Chris Rodriguez <info@premiumwd.com>
2018-11-06 21:39:15 -0800

    categories

--- src/app/layout/pages/category/category.component.ts
+++ src/app/layout/pages/category/category.component.ts
@@ -1,39 +1,46 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 // SEO
-import { Title, Meta } from "@angular/platform-browser";
+import { Title, Meta } from '@angular/platform-browser';
 
 // HTTP
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 // INTERFACE
-import { Item } from "src/app/core";
+import { Item } from 'src/app/core';
 
 
 // COMPONENT
 @Component({
-	selector: 'category-app',
-	templateUrl: 'category.component.html',
+  selector: 'category-app',
+  templateUrl: 'category.component.html',
 })
 
 // CLASS
 export class CategoryComponent implements OnInit {
 
-	// ITEMS
-	products: Item;
+  // ITEMS
+  products: Item;
 
-	constructor(title: Title, meta: Meta, private http: HttpClient) {
-		title.setTitle('Drag and Drop Interface - Vicky\'s');
-		meta.addTags([
-			{ name: 'description', content: 'Making it simple and easy drag and drop products into cart.' },
-		]);
+  // CATEGORIES
+  categories: any;
 
-		this.http.get('/assets/json/products.json').subscribe(r => {
-			this.products = r['Products'];
-		});
+  constructor(title: Title, meta: Meta, private http: HttpClient) {
+    title.setTitle('Drag and Drop Interface - Vicky\'s');
+    meta.addTags([
+      { name: 'description', content: 'Making it simple and easy drag and drop products into cart.' },
+    ]);
 
-	}
-	ngOnInit(): void {
-	}
+    this.http.get('/assets/json/products.json').subscribe(r => {
+      this.products = r['Products'];
+
+      for (const i of ['Products']) {
+        this.categories.push(i['Category']);
+      }
+    });
+
+  }
+  ngOnInit(): void {
+  }
 
 }
